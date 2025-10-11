f0e1e45f9739ec1f1366cd951b2aaf9010cd5c39
Sophie Le <66530939+LemonSCoder@users.noreply.github.com>
2025-10-10 23:36:14 -0700

    Create MadLibsImproved.java

---
+++ MadLibsImproved.java
@@ -0,0 +1,77 @@
+import java.util.Scanner;
+public class MadLibsImproved {
+    public int k = 0;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Input a Mad Lib (for any input, the type of input should be in angle brackets ex. <noun>): ");
+        String originalStory = sc.nextLine();
+        System.out.println(generateStory(originalStory));
+        sc.close();
+    }
+    public static String generateStory(String originalStory) {
+        String finalStory = "";
+        Scanner sc = new Scanner(System.in);
+        for (int i = 0; i < originalStory.length(); i++) {
+            //if (originalStory.indexOf("<") == i) {
+            try {
+                if (i + 6 <= originalStory.length()) {
+                    if (originalStory.substring(i, i + 6).equals("<noun>")) {
+                        System.out.print("Input a noun: ");
+                        String newNoun = sc.nextLine();
+                        finalStory += originalStory.substring(compareStories(originalStory, finalStory, i, k, "Noun"), i) + newNoun;
+                    } else if (originalStory.substring(i, i + 6).equals("<verb>")) {
+                        System.out.print("Input a verb: ");
+                        String newVerb = sc.nextLine();
+                        finalStory += originalStory.substring(compareStories(originalStory, finalStory, i, k, "Verb"), i) + newVerb;
+                    } else if (originalStory.substring(i, i+11).equals("<adjective>")) {
+                        System.out.print("Input an adjective: ");
+                        String newAdjective = sc.nextLine();
+                        //I'll fix this later.
+                        finalStory += originalStory.substring(compareStories(originalStory, finalStory, i, k, "Adjective"), i) + newAdjective;
+                    } else if (originalStory.substring(i, i+7).equals("<adverb>")) {
+                        System.out.print("Input an adverb: ");
+                        String newAdverb = sc.nextLine();
+                        //I'll fix this later.
+                        finalStory += originalStory.substring(compareStories(originalStory, finalStory, i, k, "Adverb"), i) + newAdverb;
+                    } else if (originalStory.substring(i, i+11).equals("<plural noun>")) {
+                        System.out.print("Input a plural noun: ");
+                        String newPluralNoun = sc.nextLine();
+                        //I'll fix this later.
+                        finalStory += originalStory.substring(compareStories(originalStory, finalStory, i, k, "Plural Noun"), i) + newPluralNoun;
+                    }
+                }
+            } catch (Exception e) {
+                ;
+            }
+        }
+        //}
+        sc.close();
+        return finalStory;
+    }
+    public static int compareStories(String originalStory, String finalStory, int index, int k, String partOfSpeech) {
+        int storyLength = originalStory.substring(k, index).length();
+        int returnVal = 0;
+        for (int j = storyLength; j < originalStory.length(); j--) {
+            if (originalStory.substring(k, index).indexOf(">") == j) {
+                returnVal = j + 1;
+            }
+        }
+        // System.out.println(returnVal);
+        return returnVal;
+    }
+    public static int kVal(String originalStory, String finalStory, int index, int k, String partOfSpeech) {
+        int storyLength = originalStory.substring(k, index).length();
+        int returnVal = 0;
+        int addVal = 0;
+        if (partOfSpeech.equals("Noun")) {
+            addVal += 6;
+        }
+        for (int j = storyLength; j < originalStory.length(); j--) {
+            if (originalStory.substring(k, index).indexOf(">") == j) {
+                k += originalStory.substring(k, index + addVal).length();
+            }
+        }
+        // System.out.println(returnVal);
+        return k;
+    }
+}
