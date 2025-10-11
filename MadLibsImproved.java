877eefcb1289fcdffca5832ae6ef55c7705e10fb
Sophie Le <66530939+LemonSCoder@users.noreply.github.com>
2025-10-10 23:39:24 -0700

    Update MadLibsImproved.java

--- MadLibsImproved.java
+++ MadLibsImproved.java
@@ -1,6 +1,5 @@
 import java.util.Scanner;
 public class MadLibsImproved {
-    public int k = 0;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Input a Mad Lib (for any input, the type of input should be in angle brackets ex. <noun>): ");
@@ -18,26 +17,26 @@ public class MadLibsImproved {
                     if (originalStory.substring(i, i + 6).equals("<noun>")) {
                         System.out.print("Input a noun: ");
                         String newNoun = sc.nextLine();
-                        finalStory += originalStory.substring(compareStories(originalStory, finalStory, i, k, "Noun"), i) + newNoun;
+                        finalStory += originalStory.substring(compareStories(originalStory, finalStory, i), i) + newNoun;
                     } else if (originalStory.substring(i, i + 6).equals("<verb>")) {
                         System.out.print("Input a verb: ");
                         String newVerb = sc.nextLine();
-                        finalStory += originalStory.substring(compareStories(originalStory, finalStory, i, k, "Verb"), i) + newVerb;
+                        finalStory += originalStory.substring(compareStories(originalStory, finalStory, i), i) + newVerb;
                     } else if (originalStory.substring(i, i+11).equals("<adjective>")) {
                         System.out.print("Input an adjective: ");
                         String newAdjective = sc.nextLine();
                         //I'll fix this later.
-                        finalStory += originalStory.substring(compareStories(originalStory, finalStory, i, k, "Adjective"), i) + newAdjective;
+                        finalStory += originalStory.substring(compareStories(originalStory, finalStory, i), i) + newAdjective;
                     } else if (originalStory.substring(i, i+7).equals("<adverb>")) {
                         System.out.print("Input an adverb: ");
                         String newAdverb = sc.nextLine();
                         //I'll fix this later.
-                        finalStory += originalStory.substring(compareStories(originalStory, finalStory, i, k, "Adverb"), i) + newAdverb;
+                        finalStory += originalStory.substring(compareStories(originalStory, finalStory, i), i) + newAdverb;
                     } else if (originalStory.substring(i, i+11).equals("<plural noun>")) {
                         System.out.print("Input a plural noun: ");
                         String newPluralNoun = sc.nextLine();
                         //I'll fix this later.
-                        finalStory += originalStory.substring(compareStories(originalStory, finalStory, i, k, "Plural Noun"), i) + newPluralNoun;
+                        finalStory += originalStory.substring(compareStories(originalStory, finalStory, i), i) + newPluralNoun;
                     }
                 }
             } catch (Exception e) {
@@ -48,30 +47,13 @@ public class MadLibsImproved {
         sc.close();
         return finalStory;
     }
-    public static int compareStories(String originalStory, String finalStory, int index, int k, String partOfSpeech) {
-        int storyLength = originalStory.substring(k, index).length();
-        int returnVal = 0;
+    public static int compareStories(String originalStory, String finalStory, int index) {
+        int storyLength = originalStory.substring(0, index).length();
         for (int j = storyLength; j < originalStory.length(); j--) {
-            if (originalStory.substring(k, index).indexOf(">") == j) {
-                returnVal = j + 1;
+            if (originalStory.substring(0, index).indexOf(">") == j) {
+                return j+1;
             }
         }
-        // System.out.println(returnVal);
-        return returnVal;
-    }
-    public static int kVal(String originalStory, String finalStory, int index, int k, String partOfSpeech) {
-        int storyLength = originalStory.substring(k, index).length();
-        int returnVal = 0;
-        int addVal = 0;
-        if (partOfSpeech.equals("Noun")) {
-            addVal += 6;
-        }
-        for (int j = storyLength; j < originalStory.length(); j--) {
-            if (originalStory.substring(k, index).indexOf(">") == j) {
-                k += originalStory.substring(k, index + addVal).length();
-            }
-        }
-        // System.out.println(returnVal);
-        return k;
+        return originalStory.substring(0, index).length();
     }
 }
